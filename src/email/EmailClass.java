3124b750a46730db38baf518ef7134bfb5b61297
Max Home <MC170171@reigate.ac.uk>
2018-01-30 18:05:32 +0000

    Fixed indentation.

--- src/email/EmailClass.java
+++ src/email/EmailClass.java
@@ -10,47 +10,46 @@ import javax.mail.Store;
 
 public class EmailClass {
 
-   public static void check(String host, String storeType, String user, String password) {
-      try {
+    public static void check(String host, String storeType, String user, String password) {
+        try {
 
-      Properties properties = new Properties();
+        Properties properties = new Properties();
 
-      properties.put("mail.pop3.host", host);
-      properties.put("mail.pop3.port", "995");
-      properties.put("mail.pop3.starttls.enable", "true");
-      Session emailSession = Session.getDefaultInstance(properties);
-  
-      Store store = emailSession.getStore("pop3s");
+        properties.put("mail.pop3.host", host);
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(properties);
 
-      store.connect(host, user, password);
+        Store store = emailSession.getStore("pop3s");
 
-      Folder emailFolder = store.getFolder("INBOX");
-      emailFolder.open(Folder.READ_ONLY);
+        store.connect(host, user, password);
 
-      Message[] messages = emailFolder.getMessages();
-      System.out.println("messages.length---" + messages.length);
+        Folder emailFolder = store.getFolder("INBOX");
+        emailFolder.open(Folder.READ_ONLY);
 
-      for (int i = 0, n = messages.length; i < n; i++) {
-         Message message = messages[i];
-         System.out.println("---------------------------------");
-         System.out.println("Email Number " + (i + 1));
-         System.out.println("Subject: " + message.getSubject());
-         System.out.println("From: " + message.getFrom()[0]);
-         System.out.println("Text: " + message.getContent().toString());
+        Message[] messages = emailFolder.getMessages();
+        System.out.println("messages.length---" + messages.length);
 
-      }
+        for (int i = 0, n = messages.length; i < n; i++) {
+            Message message = messages[i];
+            System.out.println("---------------------------------");
+             System.out.println("Email Number " + (i + 1));
+            System.out.println("Subject: " + message.getSubject());
+            System.out.println("From: " + message.getFrom()[0]);
+            System.out.println("Text: " + message.getContent().toString());
+        }
 
-      emailFolder.close(false);
-      store.close();
+        emailFolder.close(false);
+        store.close();
 
-      } catch (NoSuchProviderException e) {
-         e.printStackTrace();
-      } catch (MessagingException e) {
-         e.printStackTrace();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 
